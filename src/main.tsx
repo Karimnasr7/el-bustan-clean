46721dfdc43171df91b5997663024665cb8bd1d6
karim dev <karimdev671@gmail.com>
2025-12-17 20:33:08 +0200

    Integrate Google Tag Manager (GTM) using react-gtm-module

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,16 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
+import TagManager from 'react-gtm-module' 
 import './index.css'
 import App from './App.tsx'
+
+// 2. إعداد الحاوية بالمعرف الخاص بك
+const tagManagerArgs = {
+  gtmId: 'GTM-K39QDH6M'
+};
+
+// 3. التشغيل
+TagManager.initialize(tagManagerArgs);
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
